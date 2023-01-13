9d00f100b61dec1a8f45fb2006d2ceccd08858d6
kbu715 <kbu715@gmail.com>
2023-01-13 10:30:17 +0900

    feat: Add useLocalStorage hook

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+export const useLocalStorage = (key: string, defaultValue: string | null): [string | null, (newValue: string | null) => void] => {
+  const [storedValue, setStoredValue] = useState<string | null>(() => {
+    try {
+      const value = window.localStorage.getItem(key);
+      if (value) {
+        return JSON.parse(value);
+      } else {
+        window.localStorage.setItem(key, JSON.stringify(defaultValue));
+        return defaultValue;
+      }
+    } catch (error) {
+      return defaultValue;
+    }
+  });
+
+  const setValue = (newValue: string | null) => {
+    if (typeof newValue === 'string') {
+      try {
+      window.localStorage.setItem(key, JSON.stringify(newValue));
+      } catch (error) {
+        console.log('setValue error:' + error);
+      }
+    }
+    setStoredValue(newValue);
+  }
+
+  return [storedValue, setValue];
+}
