fc23495fb28e331af7489a18dd9dd558c3955c09
kbu715 <kbu715@gmail.com>
2023-01-13 16:36:49 +0900

    design: Implement Sign-in UI with tailwindcss

--- src/layouts/UnProtectedLayout.tsx
+++ src/layouts/UnProtectedLayout.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from 'contexts/AuthContext';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const UnProtectedLayout = () => {
@@ -11,7 +11,7 @@ const UnProtectedLayout = () => {
 
   return (
     <div>
-      <nav>
+      {/* <nav>
         <NavLink
           to="/auth/sign-in"
           className={({ isActive }) => (isActive ? 'nav-active' : undefined)}>
@@ -22,8 +22,21 @@ const UnProtectedLayout = () => {
           className={({ isActive }) => (isActive ? 'nav-active' : undefined)}>
           Sign Up
         </NavLink>
-      </nav>
-      <Outlet />
+      </nav> */}
+      <section className="h-screen flex justify-center items-center">
+        <div className="container px-6 py-12 h-full">
+          <div className="flex justify-center items-center flex-wrap h-full text-gray-800">
+            <div className="md:w-12/12 lg:w-6/12 mb-12">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+                className="w-full"
+                alt="Login Image"
+              />
+            </div>
+            <div className="md:w-8/12 lg:w-6/12">{<Outlet />}</div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,5 +1,114 @@
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <form>
+      <div className="mb-6">
+        <input
+          type="text"
+          className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-700 focus:outline-none"
+          placeholder="Email"
+        />
+      </div>
+
+      <div className="mb-6">
+        <input
+          type="password"
+          className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-700 focus:outline-none"
+          placeholder="Password"
+        />
+      </div>
+
+      <div className="flex justify-between items-center mb-6">
+        <div className="form-group form-check">
+          <input
+            type="checkbox"
+            className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+            id="checkbox"
+            defaultChecked
+          />
+          <label className="form-check-label inline-block text-gray-800" htmlFor="checkbox">
+            Remember me
+          </label>
+        </div>
+        <a
+          href="#!"
+          className="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out">
+          Forgot password?
+        </a>
+      </div>
+
+      <button
+        type="submit"
+        className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+        data-mdb-ripple="true"
+        data-mdb-ripple-color="light">
+        Sign in
+      </button>
+
+      <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+        <p className="text-center font-semibold mx-4 mb-0">OR</p>
+      </div>
+
+      <a
+        className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
+        style={{
+          backgroundColor: '#0D1117',
+        }}
+        href="#!"
+        role="button"
+        data-mdb-ripple="true"
+        data-mdb-ripple-color="light">
+        <svg
+          width="30"
+          height="30"
+          viewBox="0 -0.5 24 24"
+          id="meteor-icon-kit__regular-github"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M12.2047 0.00001C6.56031 -0.005731 1.74628 4.08615 0.842541 9.6577C-0.061195 15.2293 3.2126 20.6331 8.56941 22.4118C9.14823 22.5177 9.35294 22.1577 9.35294 21.8541C9.35294 21.5506 9.35294 20.8588 9.35294 19.8988C6.14117 20.5977 5.46353 18.3529 5.46353 18.3529C5.25046 17.6572 4.79779 17.0595 4.18588 16.6659C3.14823 15.96 4.27059 15.96 4.27059 15.96C5.00761 16.0641 5.65578 16.5014 6.02823 17.1459C6.34368 17.7179 6.87393 18.1406 7.50179 18.3208C8.12965 18.5009 8.8034 18.4236 9.37411 18.1059C9.41842 17.5252 9.66876 16.9794 10.08 16.5671C7.5247 16.2777 4.84235 15.2894 4.84235 10.92C4.82481 9.7786 5.24688 8.67412 6.02117 7.8353C5.67632 6.84285 5.71662 5.7571 6.13412 4.79295C6.13412 4.79295 7.10117 4.48236 9.29647 5.97177C11.1816 5.45419 13.1713 5.45419 15.0565 5.97177C17.2518 4.48236 18.2118 4.79295 18.2118 4.79295C18.6351 5.74689 18.6854 6.82486 18.3529 7.81412C19.1272 8.65294 19.5493 9.7574 19.5318 10.8988C19.5318 15.3177 16.8424 16.2847 14.28 16.5459C14.8359 17.1047 15.1218 17.8774 15.0635 18.6635C15.0635 20.2024 15.0635 21.4447 15.0635 21.8188C15.0635 22.1929 15.2682 22.4824 15.8541 22.3694C21.1473 20.5447 24.3569 15.1728 23.4554 9.6469C22.5539 4.1211 17.8034 0.04779 12.2047 0.00001z"
+            fill="#758CA3"
+          />
+        </svg>
+        <span className="ml-2">Continue with Github</span>
+      </a>
+      <a
+        className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center"
+        style={{
+          backgroundColor: '#db4437',
+        }}
+        href="#!"
+        role="button"
+        data-mdb-ripple="true"
+        data-mdb-ripple-color="light">
+        <svg
+          width="30"
+          height="30"
+          viewBox="0 0 32 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M30.0014 16.3109C30.0014 15.1598 29.9061 14.3198 29.6998 13.4487H16.2871V18.6442H24.1601C24.0014 19.9354 23.1442 21.8798 21.2394 23.1864L21.2127 23.3604L25.4536 26.58L25.7474 26.6087C28.4458 24.1665 30.0014 20.5731 30.0014 16.3109Z"
+            fill="#4285F4"
+          />
+          <path
+            d="M16.2863 29.9998C20.1434 29.9998 23.3814 28.7553 25.7466 26.6086L21.2386 23.1863C20.0323 24.0108 18.4132 24.5863 16.2863 24.5863C12.5086 24.5863 9.30225 22.1441 8.15929 18.7686L7.99176 18.7825L3.58208 22.127L3.52441 22.2841C5.87359 26.8574 10.699 29.9998 16.2863 29.9998Z"
+            fill="#34A853"
+          />
+          <path
+            d="M8.15964 18.769C7.85806 17.8979 7.68352 16.9645 7.68352 16.0001C7.68352 15.0356 7.85806 14.1023 8.14377 13.2312L8.13578 13.0456L3.67083 9.64746L3.52475 9.71556C2.55654 11.6134 2.00098 13.7445 2.00098 16.0001C2.00098 18.2556 2.55654 20.3867 3.52475 22.2845L8.15964 18.769Z"
+            fill="#FBBC05"
+          />
+          <path
+            d="M16.2864 7.4133C18.9689 7.4133 20.7784 8.54885 21.8102 9.4978L25.8419 5.64C23.3658 3.38445 20.1435 2 16.2864 2C10.699 2 5.8736 5.1422 3.52441 9.71549L8.14345 13.2311C9.30229 9.85555 12.5086 7.4133 16.2864 7.4133Z"
+            fill="#EB4335"
+          />
+        </svg>
+        <span className="ml-2">Continue with Google</span>
+      </a>
+    </form>
+  );
 };
 
 export default SignIn;
